f1e3912bed6ec651dd0150bf9db486f0ce39e540
Scott Talbert <swt@techie.net>
2014-03-29 13:20:02 -0400

    Update mkrelease.sh script to use 'git archive'

--- mkrelease.sh
+++ mkrelease.sh
@@ -1,43 +1,10 @@
 #!/bin/sh
 
-# This code NOT copyright Stephen Warren <s-t-concordance@wwwdotorg.org>
-# This code is released into the public domain.
+if [ "$#" -ne 1 ]; then
+    echo "Usage: $0 <git tag, e.g. congruity-18>"
+    exit 1
+fi
+VERSION=$1
 
-cd `dirname $0`
-
-name=congruity
-version=`./congruity --version`
-
-name_ver=${name}-${version}
-tar_name=${name_ver}.tar.bz2
-rel_path=releases
-tar_path=${rel_path}/${tar_name}
-
-echo Releasing version ${name_ver}
-
-rm -rf ${name_ver}
-mkdir ${name_ver}
-cp -rp \
-  congruity \
-  harmony.wsdl \
-  mhgui \
-  mhgui.1 \
-  mhgui.desktop \
-  mhmanager.py \
-  *.gif \
-  *.png \
-  *.xsd \
-  Changelog \
-  congruity.1 \
-  congruity.desktop \
-  COPYING \
-  LICENSE.txt \
-  Makefile \
-  README.txt \
-  ${name_ver}
-
-rm -rf ${tar_path}
-mkdir -p ${rel_path}
-tar jcvf ${tar_path} ${name_ver}
-rm -rf ${name_ver}
+git archive --format=tar --prefix=${VERSION}/ ${VERSION} | bzip2 > ${VERSION}.tar.bz2
 
